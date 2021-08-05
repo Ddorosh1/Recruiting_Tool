b7d634a1ec8b0c8cbcf77bb74e36a45e854642f5
ddorosh1 <doroshdmitriy@gmail.com>
2021-08-04 17:29:37 -0700

    https://github.com/Ddorosh1/Recruiting_Tool/

---
+++ main/apps/myproject/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'myproject/index.html')
+
+def dashboard(request):
+    return render(request, 'myproject/dashboard.html')
+
+def positions(request):
+    return render(request, 'myproject/positions.html')
